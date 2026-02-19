068ff8f2af4f0b151281fb4a9a6aea0181127837
skybbk1001 <skybbk1001@users.noreply.github.com>
2026-02-20 02:35:55 +0800

    deploy: skybbk1001/skybook-hexo@6b34b3dbae11bc4cdb0ba9fcceb5068a9d6fc686

--- js/theme/back-to-top.js
+++ js/theme/back-to-top.js
@@ -1,15 +1,1 @@
-(function () {
-  "use strict";
-
-  Theme.backToTop = {
-    register: function () {
-      document.getElementById('back-top')
-        .addEventListener('click', () => {
-          window.scroll({
-            top: 0,
-            behavior: "smooth"
-          });
-        });
-    }
-  };
-}.call(this));
+(function(){"use strict";Theme.backToTop={register:function(){document.getElementById("back-top").addEventListener("click",()=>{window.scroll({top:0,behavior:"smooth"})})}}}).call(this);
--- js/theme/smooth-corners.js
+++ js/theme/smooth-corners.js
@@ -1,11 +1,1 @@
-(function () {
-  "use strict";
-
-  Theme.smoothCorners = {
-    register: function () {
-      if (CSS && 'paintWorklet' in CSS)
-        CSS.paintWorklet.addModule(
-          window.config.url_root + 'js/smooth-corners.js');
-    }
-  };
-}.call(this));
+(function(){"use strict";Theme.smoothCorners={register:function(){CSS&&"paintWorklet"in CSS&&CSS.paintWorklet.addModule(window.config.url_root+"js/smooth-corners.js")}}}).call(this);
